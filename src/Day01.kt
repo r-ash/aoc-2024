c715416971e46b6f3fc172af7133800208282ee0
Rob Ashton <robertashton94@gmail.com>
2024-12-01 06:36:35 +0000

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,38 @@
+import kotlin.math.abs
+import kotlin.time.measureTime
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val list1 = input.map{ it.split("   ").first().toInt() }
+        val list2 = input.map{ it.split("   ").last().toInt() }
+
+        return list1.sorted().zip(list2.sorted()).fold(0){acc, pair ->
+            acc + abs(pair.first - pair.second)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = input.map{ it.split("   ").first().toInt() }
+        val counts = input
+            .map{ it.split("   ").last().toInt() }
+            .groupingBy { it }.eachCount()
+
+        return list1.fold(0) {acc, value ->
+            acc + (value * counts.getOrDefault(value, 0))
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val part1Time = measureTime {
+        part1(input).println()
+    }
+    println("Part 1 took: $part1Time")
+    val part2Time = measureTime {
+        part2(input).println()
+    }
+    println("Part 2 took: $part2Time")
 }
